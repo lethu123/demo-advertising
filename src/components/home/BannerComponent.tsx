05c1c8ae91df83b6e4c14d8a677d1f9049b86d08
lethithu <lethu210398@gmail.com>
2024-03-22 00:50:06 +0700

    fix build

--- src/components/home/BannerComponent.tsx
+++ src/components/home/BannerComponent.tsx
@@ -1,10 +1,11 @@
+"use client"
+
 import React, { useEffect } from 'react'
 import homecss from '@assets/scss/home.module.scss'
 import Link from 'next/link'
 import logo from '@assets/images/logo.svg'
 import MenuMobile from './MenuMobile'
 import Image from 'next/image'
-import Container from '@/commons/Container'
 
 
 const BannerComponent = () => {
