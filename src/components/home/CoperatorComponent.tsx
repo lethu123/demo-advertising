1dd5fc30903303c099abe5951243b2f1a9e3e66a
lethu <lethu210398@gmail.com>
2025-07-01 23:07:08 +0700

    update logo coperator

--- src/components/home/CoperatorComponent.tsx
+++ src/components/home/CoperatorComponent.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import React from "react";
 import "@assets/scss/home.scss";
-import logo1 from "@assets/images/logo1.svg";
-import logo2 from "@assets/images/logo2.svg";
-import logo3 from "@assets/images/logo3.svg";
-import logo4 from "@assets/images/logo4.svg";
+import logo1 from "@assets/images/logo1.png";
+import logo2 from "@assets/images/logo2.png";
+import logo3 from "@assets/images/logo3.png";
+import logo4 from "@assets/images/logo4.png";
+import logo5 from "@assets/images/logo5.png";
+import logo6 from "@assets/images/logo6.png";
+import logo7 from "@assets/images/logo7.png";
+import logo8 from "@assets/images/logo8.png";
 import Container from "@/commons/Container";
 import dynamic from "next/dynamic";
 
@@ -31,46 +35,67 @@ const CoperatorComponent = () => {
       >
         <div className="w-full xl:w-4/5 max-w-screen-xl mx-auto">
           <div className="w-full flex gap-x-[2rem] gap-y-[1rem] items-end  flex-wrap justify-center">
-            <Image
-              alt=""
-              src={logo1}
-              className="w-[3rem] md:w-[4rem] lg:w-[6rem]"
-            />
-            <Image
-              alt=""
-              src={logo2}
-              className="w-[3rem] md:w-[4rem] lg:w-[6rem]"
-            />
-            <Image
-              alt=""
-              src={logo3}
-              className="w-[3rem] md:w-[4rem] lg:w-[6rem]"
-            />
-            <Image
-              alt=""
-              src={logo4}
-              className="w-[3rem] md:w-[4rem] lg:w-[6rem]"
-            />
-            <Image
-              alt=""
-              src={logo2}
-              className="w-[3rem] md:w-[4rem] lg:w-[6rem]"
-            />
-            <Image
-              alt=""
-              src={logo3}
-              className="w-[3rem] md:w-[4rem] lg:w-[6rem]"
-            />
-            <Image
-              alt=""
-              src={logo1}
-              className="w-[3rem] md:w-[4rem] lg:w-[6rem]"
-            />
-            <Image
-              alt=""
-              src={logo4}
-              className="w-[3rem] md:w-[4rem] lg:w-[6rem]"
-            />
+            <div className="bg-white">
+              <Image
+                alt=""
+                src={logo1}
+                className="w-[3rem] md:w-[4rem] lg:w-[6rem]"
+              />
+            </div>
+            <div className="bg-white flex items-center justify-center p-1">
+              <Image
+                alt=""
+                src={logo2}
+                className="w-[3rem] md:w-[4rem] lg:w-[6rem]"
+              />
+            </div>
+            <div className="bg-white flex items-center justify-center p-1">
+              <Image
+                alt=""
+                src={logo3}
+                className="w-[3rem] md:w-[4rem] lg:w-[6rem]"
+              />
+            </div>
+
+            <div className="bg-white flex items-center justify-center p-1">
+              <Image
+                alt=""
+                src={logo4}
+                className="w-[3rem] md:w-[4rem] lg:w-[6rem]"
+              />
+            </div>
+
+            <div className="bg-white flex items-center justify-center p-1">
+              <Image
+                alt=""
+                src={logo5}
+                className="w-[3rem] md:w-[4rem] lg:w-[6rem]"
+              />
+            </div>
+
+            <div className="bg-white flex items-center justify-center p-1">
+              <Image
+                alt=""
+                src={logo6}
+                className="w-[3rem] md:w-[4rem] lg:w-[6rem]"
+              />
+            </div>
+
+            <div className="bg-white flex items-center justify-center p-1">
+              <Image
+                alt=""
+                src={logo7}
+                className="w-[3rem] md:w-[4rem] lg:w-[6rem]"
+              />
+            </div>
+
+            <div className="bg-white flex items-center justify-center p-1">
+              <Image
+                alt=""
+                src={logo8}
+                className="w-[3rem] md:w-[4rem] lg:w-[6rem]"
+              />
+            </div>
           </div>
         </div>
       </div>
